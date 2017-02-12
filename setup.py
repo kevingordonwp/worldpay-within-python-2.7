86fcc5cca13fb767960ce1991ec21381904b82c7
kevingordonwp <kevin.gordon@worldpay.com>
2017-02-12 02:14:12 -0500

    Added code to get submodules

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 
 import os
 
+os.system("git submodule init")
+os.system("git submoduel update")
 # assume this works for now
 os.system("sudo python ./thrift/lib/py/setup.py install")
 print "Assumes thrift installed okay for now..."
