af9c053d611f83f1bd105b60542bab8cacb6785a
kevingordonwp <kevin.gordon@worldpay.com>
2017-02-12 03:11:52 -0500

    updating setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import os
 os.system("git submodule init")
 os.system("git submoduel update")
 # assume this works for now
-os.system("sudo python ./thrift/lib/py/setup.py install")
+os.system("cd ./thrift/lib/py/; sudo python setup.py install")
 print "Assumes thrift installed okay for now..."
-os.system("cat README.txt")
+os.system("cat README.md")
